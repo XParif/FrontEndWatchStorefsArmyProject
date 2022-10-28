5f123928ae292b9187cc164e0b2fe616a007fcbd
XParif <arifsdarif@gmail.com>
2022-10-28 20:04:27 +0600

    slider added

--- components/home/index.jsx
+++ components/home/index.jsx
@@ -1,11 +1,13 @@
-import WeeklyProducts from './weeklyProducts'
-import SummerSale from './summerSale'
-import SpecialFeatures from './specialFeatures'
-import MostPremiumWatch from './mostPremiumWatch'
+import WeeklyProducts from "./weeklyProducts";
+import SummerSale from "./summerSale";
+import SpecialFeatures from "./specialFeatures";
+import MostPremiumWatch from "./mostPremiumWatch";
+import Slider from "./slider";
 
 const HomgePage = () => {
   return (
     <>
+      <Slider />
       <WeeklyProducts />
       <SummerSale />
       <SpecialFeatures />
