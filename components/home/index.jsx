a739dc5881ae46e61b56eaabe943f88b734ed7da
XParif <arifsdarif@gmail.com>
2022-10-28 18:59:58 +0600

    all home component moved into HomePage

---
+++ components/home/index.jsx
@@ -0,0 +1,17 @@
+import WeeklyProducts from './weeklyProducts'
+import SummerSale from './summerSale'
+import SpecialFeatures from './specialFeatures'
+import MostPremiumWatch from './mostPremiumWatch'
+
+const HomgePage = () => {
+  return (
+    <>
+      <WeeklyProducts />
+      <SummerSale />
+      <SpecialFeatures />
+      <MostPremiumWatch />
+    </>
+  );
+};
+
+export default HomgePage;
