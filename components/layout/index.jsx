a6aed50170e95bcf8d1fc055bc202952481dd0ce
MD. Ariful Islam <41154962+arif-btkrm@users.noreply.github.com>
2022-10-29 14:45:20 +0600

    solved module directory issue

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import GlobalStyle from "../components/globalStyles";
-import theme from "./../theme/";
-import Footer from "./footer";
-import Header from "./Header";
-import Modal from "./modal";
+import theme from "../../theme";
+import GlobalStyle from "../globalStyles";
+// import Footer from "./footer";
+// import Header from "./Header";
+// import Modal from "./modal";
 
 export default function Layout({ children }) {
   const [openModal, setOpenModal] = useState(false);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      {openModal && <Modal modalController={setOpenModal} />}
-      <Header modalController={setOpenModal} />
+      {/* {openModal && <Modal modalController={setOpenModal} />} */}
+      {/* <Header modalController={setOpenModal} /> */}
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </ThemeProvider>
   );
 }
