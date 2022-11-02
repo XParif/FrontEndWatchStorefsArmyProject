35a62a3fc27511e11db6d5cf17358f8dae6383c6
XParif <arifsdarif@gmail.com>
2022-11-02 20:21:39 +0600

    404 page added

---
+++ components/404/ErrorCode.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ErrorCode = styled.div`
+  font-size: 10rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme?.color?.primary};
+`;
+
+export default ErrorCode
