0ed21fd9955d3009985e73f49d0dcb4ae88d8775
XParif <arifsdarif@gmail.com>
2022-11-16 23:06:45 +0600

    some code updated

--- components/collection/pageBody/categorySection/Category.jsx
+++ components/collection/pageBody/categorySection/Category.jsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 import styled from "styled-components";
 import Checkbox from "./../../../shared/checkboxes/index";
 import Title from "./../../../shared/texts/Title";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import BlockText from "../../../shared/texts/BlockText";
+import { FaCheck } from "react-icons/fa";
 
 const CheckboxGroupStyle = styled.div`
   display: flex;
   flex-direction: column;
-  gap: .75rem;
+  gap: 0.75rem;
   margin: 1rem 0;
 `;
 
@@ -18,68 +19,63 @@ const CategoryStyle = styled.div`
 
 const CheckBoxDesign = styled.div`
   display: flex;
-  gap: .75rem;
-`
-
-const CheckedBox = styled.div`
-  width: .75rem;
-  height: .75rem;
-  background-color: ${({theme}) => theme?.color?.primary};
-  border: 2px solid white;
-  border-radius: 15px;
-  outline: 2px solid ${({theme}) => theme?.color?.primary};
-`
+  align-items: center;
+  gap: 0.75rem;
+`;
 
 const Box = styled.div`
-  width: .75rem;
-  height: .75rem;
+  width: 1.25rem;
+  height: 1.25rem;
   border: 2px solid white;
-  border-radius: 15px;
-  outline: 2px solid ${({theme}) => theme?.color?.primary};
-`
+  border-radius: 1px;
+  outline: 2px solid ${({ theme }) => theme?.color?.primary};
 
-const Category = ({sorting , title, list , qureParamsArray }) => {
-  qureParamsArray = qureParamsArray.split('+')
-  const [uiStateQureParamsArray , setUiStateQureParamsArray] = useState([...qureParamsArray]);
-  
-  const [qureObj , setQureObj] =   useState(list.reduce((acc , cu)=>{
-    acc[`${cu}`] =  qureParamsArray.find(v => v == cu) ? true : false;
-    return acc
-},{}))
+  * {
+    color: ${({theme}) => theme?.color?.primary}
+  }
+`;
 
-  console.log(qureObj)
+const Category = ({ sorting, title, list, qureParamsArray }) => {
+  qureParamsArray = qureParamsArray.split("+");
+  const [uiStateQureParamsArray, setUiStateQureParamsArray] = useState([
+    ...qureParamsArray,
+  ]);
 
-  const reFIneUiStateQureParamsArray = (item)=>{
-    if(!qureObj[`${item}`]){
+  const [qureObj, setQureObj] = useState(
+    list.reduce((acc, cu) => {
+      acc[`${cu}`] = qureParamsArray.find((v) => v == cu) ? true : false;
+      return acc;
+    }, {})
+  );
 
-      setUiStateQureParamsArray((prv)=>{
-        return [...prv , item]
-      })
+  console.log(qureObj);
+
+  const reFIneUiStateQureParamsArray = (item) => {
+    if (!qureObj[`${item}`]) {
+      setUiStateQureParamsArray((prv) => {
+        return [...prv, item];
+      });
 
       setQureObj((prv) => {
         prv[`${item}`] = true;
         return {
-        ...prv,
-      }
-      })
-
-    }else{
-
-      setUiStateQureParamsArray((prv)=>{
-        prv = prv.filter(v=> v !== item)
-        return [...prv]
-      })
+          ...prv,
+        };
+      });
+    } else {
+      setUiStateQureParamsArray((prv) => {
+        prv = prv.filter((v) => v !== item);
+        return [...prv];
+      });
 
       setQureObj((prv) => {
         prv[`${item}`] = false;
         return {
-        ...prv,
-      }
-      })
-
+          ...prv,
+        };
+      });
     }
-  }
-
+  };
 
   return (
     <CategoryStyle>
@@ -87,43 +83,52 @@ const Category = ({sorting , title, list , qureParamsArray }) => {
         {title}
       </Title>
       <CheckboxGroupStyle>
-        {list.map((item ,index) => {
+        {list.map((item, index) => {
+          const reFinequreParamsArray = [];
 
-          const reFinequreParamsArray = []; 
-
-
-          if(!qureObj[`${item}`]){
-            reFinequreParamsArray[index] = reFinequreParamsArray[index] = uiStateQureParamsArray.reduce((acc , cu)=>{
-              acc =  acc + "+" + cu   
-              return acc
-            });
-            reFinequreParamsArray[index]+= `+${item}`
-          }else{
-            reFinequreParamsArray[index] = uiStateQureParamsArray.reduce((acc , cu)=>{
-              if(cu == item){
-                return acc
+          if (!qureObj[`${item}`]) {
+            reFinequreParamsArray[index] = reFinequreParamsArray[index] =
+              uiStateQureParamsArray.reduce((acc, cu) => {
+                acc = acc + "+" + cu;
+                return acc;
+              });
+            reFinequreParamsArray[index] += `+${item}`;
+          } else {
+            reFinequreParamsArray[index] = uiStateQureParamsArray.reduce(
+              (acc, cu) => {
+                if (cu == item) {
+                  return acc;
+                }
+                acc = acc + "+" + cu;
+                return acc;
               }
-              acc =  acc + "+" +cu  
-              return acc
-            }); 
-            
+            );
           }
 
-          reFinequreParamsArray[index] = reFinequreParamsArray[index].slice(1)
+          reFinequreParamsArray[index] = reFinequreParamsArray[index].slice(1);
           // if(!Array.isArray(reFinequreParamsArray[index])){
-            
-          return(
-            <Link onClick={()=> reFIneUiStateQureParamsArray(item)}  key={index} href={{ pathname: '/collections', query: { catagories: reFinequreParamsArray[index] , sorting : sorting}}}>
+
+          return (
+            <Link
+              onClick={() => reFIneUiStateQureParamsArray(item)}
+              key={index}
+              href={{
+                pathname: "/collections",
+                query: {
+                  catagories: reFinequreParamsArray[index],
+                  sorting: sorting,
+                },
+              }}
+            >
               <CheckBoxDesign>
-               {qureObj[item] ? <CheckedBox/> : <Box />  }
-              <BlockText>{item}</BlockText>
+                {/* {qureObj[item] ? <CheckedBox/> : <Box />  } */}
+                <Box>{qureObj[item] && <FaCheck />}</Box>
+                <BlockText color={qureObj[item] && "primary"} weight={qureObj[item] && "semiBold"}>{item}</BlockText>
               </CheckBoxDesign>
               {/* <Checkbox   defaultChecked={( -1 !== uiStateQureParamsArray.findIndex(v => v == item))}  text={item} /> */}
             </Link>
-          )
-
-          })
-        }
+          );
+        })}
       </CheckboxGroupStyle>
     </CategoryStyle>
   );
