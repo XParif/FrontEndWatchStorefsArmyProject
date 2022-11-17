50fad0075a599fcce4d44d1c7cd3fabfd581845f
XParif <arifsdarif@gmail.com>
2022-11-17 15:00:12 +0600

    color css added

---
+++ components/products/Products.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const RadioButtonLabel = styled.label`
+    text-transform: capitalize;
+    margin-right: 1rem;
+    cursor: pointer;
+`
+
+export const RadioButtonInput = styled.input`
+    margin-right: .25rem;
+`
