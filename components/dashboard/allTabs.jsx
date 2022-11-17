e1404606e014465d9cc063b01d302a8486345961
XParif <arifsdarif@gmail.com>
2022-11-17 17:46:59 +0600

    dashboard updated

--- components/dashboard/allTabs.jsx
+++ components/dashboard/allTabs.jsx
@@ -1,4 +1,4 @@
-import React, { useState  , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { TabContainer, LeftSide, RightSide, Tab } from "./tab.styles";
 import {
   BsArrowDownLeftCircleFill,
@@ -12,51 +12,63 @@ import Review from "./tabComponent/review";
 import History from "./tabComponent/history";
 import Settings from "./tabComponent/settings";
 import { gql, useLazyQuery } from "@apollo/client";
+import { OuterOrderRow, InnerOrderRow } from "./dashboard.styled";
+import BlockText from "./../shared/texts/BlockText";
 
 const AllTabs = () => {
-  const [calledOrderData , {data : ordeINfo , loading : orderFetchLoading }] = useLazyQuery(gql`query{
-    orders{
-      data{
-        id,
-        attributes{
-          totalPrice,
-          isConfirmed,
-          shipingCost,
-          ordersItem{
-            id,
-            productId
-            ,productName,
-            product_quantity,variantsId,variantCode,itemsTotalPrice,unitPrice,
-          }
-          vatInPercentages,
-          delivaryAddress{
-            data{
-              id,
-              attributes{
-                address,
-                streetAddress,
-                state_Province_Region,zipCode,
-                country,
-                city
+  const [calledOrderData, { data: ordeINfo, loading: orderFetchLoading }] =
+    useLazyQuery(gql`
+      query {
+        orders {
+          data {
+            id
+            attributes {
+              totalPrice
+              isConfirmed
+              shipingCost
+              ordersItem {
+                id
+                productId
+                productName
+                product_quantity
+                variantsId
+                variantCode
+                itemsTotalPrice
+                unitPrice
               }
+              vatInPercentages
+              delivaryAddress {
+                data {
+                  id
+                  attributes {
+                    address
+                    streetAddress
+                    state_Province_Region
+                    zipCode
+                    country
+                    city
+                  }
+                }
+              }
+              paymentMethod
             }
-          },
-          paymentMethod,
+          }
         }
       }
-    }
-  }`)
-  const [active, setActive] = useState(()=>{calledOrderData() ; return 1});
-  const [content, setContent] = useState(<Receive  ordeINfo = {ordeINfo}  />);
-  useEffect(()=>{
-    setContent(  <Receive ordeINfo={ordeINfo} />  );
-  },[orderFetchLoading])
+    `);
+  const [active, setActive] = useState(() => {
+    calledOrderData();
+    return 1;
+  });
+  const [content, setContent] = useState(<Receive ordeINfo={ordeINfo} />);
+  useEffect(() => {
+    setContent(<Receive ordeINfo={ordeINfo} />);
+  }, [orderFetchLoading]);
 
   const handleClick = (index) => {
     setActive(index);
     if (index === 1) {
-      calledOrderData()
-    
+      calledOrderData();
     } else if (index === 2) {
       setContent(Return);
     } else if (index === 3) {
@@ -68,13 +80,21 @@ const AllTabs = () => {
     }
   };
 
-  
+  // console.log("content : " , content);
+
+  const orders = content?.props?.ordeINfo?.orders;
 
   return (
     <TabContainer>
       <LeftSide>
-        <Tab onClick={() => {handleClick(1) ; handleClick(1)}} active={active === 1}>
-          <FaTruck /> <div>Orders in Procces</div>
+        <Tab
+          onClick={() => {
+            handleClick(1);
+            handleClick(1);
+          }}
+          active={active === 1}
+        >
+          <FaTruck /> <div>Orders</div>
         </Tab>
         <Tab onClick={() => handleClick(2)} active={active === 2}>
           <BsArrowDownLeftCircleFill /> <div>Returns</div>
@@ -90,7 +110,57 @@ const AllTabs = () => {
         </Tab>
       </LeftSide>
 
-      <RightSide>{content}</RightSide>
+      <RightSide>
+        {orders?.map((items, index) => {
+          const {
+            address,
+            streetAddress,
+            state_Province_Region,
+            city,
+            zipCode,
+          } = items.delivaryAddress;
+          return (
+            <OuterOrderRow>
+              <BlockText size="md" weight="medium">Order ID : #{items.id}</BlockText>
+              <BlockText size="md" weight="medium">
+                Payment Method : {items.paymentMethod} Delivery
+              </BlockText>
+              <BlockText size="md" weight="medium">Total Price : {items.totalPrice}</BlockText>
+              <BlockText size="md" weight="medium">
+                Address : {streetAddress} , {address}, {state_Province_Region},{" "}
+                {city} - {zipCode}
+              </BlockText>
+
+              {items?.ordersItem?.map((item, index) => {
+                console.log(item);
+                return (
+                  <InnerOrderRow>
+                    <BlockText size="md" weight="medium">Item Order ID : #{item.id}</BlockText>
+                    <BlockText size="md" weight="medium">
+                      {" "}
+                      Product Name : {items.productName} Delivery
+                    </BlockText>
+                    <BlockText size="md" weight="medium">Item Price : {items.unitPrice}</BlockText>
+                    <BlockText size="md" weight="medium">Quantity : {items.product_quantity}</BlockText>
+                    <BlockText size="md" weight="medium">
+                      Item Total Price: {items.itemsTotalPrice}
+                    </BlockText>
+                  </InnerOrderRow>
+                );
+              })}
+              {/* {item[0]?.ordersItem?.map((innerItem, index) => (
+              <InnerOrderRow>{index}</InnerOrderRow>
+            ))} */}
+              {/* {JSON.stringify(item[0])} */}
+              {/* {console.log(item[0])} */}
+            </OuterOrderRow>
+          );
+        })}
+      </RightSide>
+      {/* <RightSide>{content.orders.map((item) => <OuterOrderRow key={item.id}>
+          i'm first order
+        </OuterOrderRow>)}
+      </RightSide> */}
     </TabContainer>
   );
 };
--- components/dashboard/tab.styles.jsx
+++ components/dashboard/tab.styles.jsx
@@ -4,7 +4,7 @@ import { justifyAlignCenter } from './../../utils/display.styled';
 export const TabContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(12, 1fr);
-  grid-gap: 3rem;
+  grid-gap: 1rem;
 `;
 
 export const LeftSide = styled.div`
@@ -14,6 +14,10 @@ export const LeftSide = styled.div`
   grid-column: 1/3;
 `;
 export const RightSide = styled.div`
+height: 100%;
+padding: 2rem 0;
+overflow-y: scroll;
+overflow-x: hidden;
   grid-column: 3/-1;
   background-color: ${({ theme }) => theme?.color?.white};
   color: ${({ theme }) => theme?.color?.text};
@@ -28,7 +32,6 @@ export const Tab = styled.a`
   cursor: pointer;
   width: 100%;
   position: relative;
-
   font-size: ${({ theme }) => theme?.fontSizes?.md};
   font-weight: ${({ theme }) => theme?.fontWeight?.medium};
   background-color: ${(props) => (props.active ? "#eaeaff" : "#f5f6f8")};
